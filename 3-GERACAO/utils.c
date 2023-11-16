2b52dfbc6ce914d67e234aa32a21644b55355cc1
pcmoraesmenezes <paulo.moraes@sou.unifal-mg.edu.br>
2023-11-16 12:02:04 -0300

    :bulb: feat: adicionando novas funcionalidades

--- 3-GERACAO/utils.c
+++ 3-GERACAO/utils.c
@@ -2,6 +2,11 @@
 
 #define TAM_TAB 100
 
+enum{
+    INT,
+    LOG
+};
+
 
 struct elemTabSimbolos {
     char id[100]; // nome do identificador 
@@ -42,6 +47,17 @@ void insereSimbolo (struct elemTabSimbolos elem){
     tabSimb[posTab++] = elem;
 }
 
+void mostraTabela (){
+    puts("Tabela de simbolos: ");
+    puts("---------------------------");
+    printf("%30s | %s | %s\n", "ID", "END", "TIP");
+    for(int i = 0; i < 50; i++)
+        printf("-");
+    for (int i = 0; i <posTab; i++){
+        printf("\n%30s | %3d | %s", tabSimb[i].id, tabSimb[i].end, tabSimb[i].tip == INT? "INT" : "LOG");
+    }
+    puts("");
+}
 // Pilha semantica
 
 #define TAM_PIL 100
